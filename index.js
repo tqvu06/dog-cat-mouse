b78b44a5e4c67c1933d6010413c471ef34e3f098
Vu Trinh <tqvu06@gmail.com>
2020-05-03 20:22:50 +0700

    Dog eats Cat

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var Dog = require('./dog');
+var Cat = require('./cat');
 
 var dog = new Dog('Luoc');
 
 dog.sayHi();
+
+var tom = new Cat('Tom', 'yellow');
+
+dog.eat(tom);
+console.log(dog);
 
 
 
